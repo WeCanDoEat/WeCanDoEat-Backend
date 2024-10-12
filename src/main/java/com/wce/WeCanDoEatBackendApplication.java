b0c0c868a557ec31c990f9c23500eb385debfe14
JHJ08 <161445093+JHJ08@users.noreply.github.com>
2024-10-12 01:10:48 -0700

    init commit

---
+++ src/main/java/com/wce/WeCanDoEatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCanDoEatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeCanDoEatBackendApplication.class, args);
+    }
+
+}
